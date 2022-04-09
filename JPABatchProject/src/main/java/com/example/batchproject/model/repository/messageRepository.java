afe8f53d44a94cbab7240576927e05705d0ebd13
beescuit9510 <pengsoon20@gmail.com>
2022-04-09 22:43:52 +0900

    fix updateIsSentTrue error

--- JPABatchProject/src/main/java/com/example/batchproject/model/repository/messageRepository.java
+++ JPABatchProject/src/main/java/com/example/batchproject/model/repository/messageRepository.java
@@ -17,8 +17,9 @@ public interface messageRepository extends JpaRepository<PushMessage, Long> {
     List<PushMessage> selectMessageToSend();
 
     @Modifying
-    @Query(value = "UPDATE push_message set is_sent = true where is_sent = 0 and p.is_sent = ?",nativeQuery = true)
-    List<PushMessage> updateIsSentTrue(Long pushMessageNo);
+    @Query(value = "UPDATE push_message set is_sent = 1 where is_sent = 0 and push_message_no = ?",nativeQuery = true)
+    void updateIsSentTrue(Long pushMessageNo);
+
 
 }
 
